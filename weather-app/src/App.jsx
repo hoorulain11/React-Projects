5a43a97cfc0be015af56e9947350e4175d18f073
hoorulain11 <hoorulainbarkatullahkhan218@gmail.com>
2024-06-23 11:39:53 +0500

    weather app

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -27,19 +27,19 @@ function App() {
   return (
     
     <div className='bg-gradient-to-r from-red-400 to-red-900 uppercase border border-purple-800 rounded-lg w-fit text-white p-4 mx-auto my-8'>
-      <h1 className='drop-shadow-lg m-3 p-3 font-extrabold text-4xl text-rose-300'>Weather Application </h1>
+      <h1 className='drop-shadow-lg m-3 p-3 font-extrabold text-4xl text-white'>Weather Application </h1>
       <form onSubmit={handleSubmit}>
-      <input className='outline-none bg-rose-200  capitalize rounded-md border text-rose-700 border-l-rose-400 p-2 mx-6 font-bold'
+      <input className='outline-none bg-rose-50-200  capitalize rounded-md border text-rose-700 border-l-rose-400 p-2 mx-10 font-bold'
           type="text" 
           placeholder="Enter city name..."
           value={city} 
           onChange={(e)=>setCity(e.target.value)}
         />
-        <button  className=' text-rose-950 bg-rose-500 outline-none border border-rose-500 active:bg-rose-300 m-4 p-2 hover:bg-rose-400 rounded-md' type="submit">Get Weather</button>
+        <button  className=' text-white font-bold bg-rose-500 outline-none border border-rose-500 active:bg-rose-300 m-4 p-2 hover:bg-rose-400 rounded-md' type="submit">Get Weather</button>
       </form>
       {error && <p className='m-8 text-rose-950'>{error}</p>}
       {weather && (
-        <div className='mx-11 p-2  text-rose-950 text-2xl  font-semibold capitalize'>
+        <div className='mx-11 p-2  text-white text-3xl  font-medium capitalize'>
           <h2 className='m-2 p-1'>{weather.name}</h2>
           <p className='m-2 p-2'>{Math.floor(weather.main.temp-273.15)}°C</p>
           <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`} width={80} height={80} />
