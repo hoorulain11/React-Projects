b0f6e3eabd4b84bc4919f78d812967586b3dba28
hoorulain11 <hoorulainbarkatullahkhan218@gmail.com>
2024-07-13 14:11:51 +0500

    quiz-app

---
+++ quiz-app/src/Components/QuestionBank.jsx
@@ -0,0 +1,40 @@
+const qBank = [
+    {
+        id: 1,
+        question: "What is the national lagnauge of Pakistan?",
+        options: ["Urdu", "Punjabi", "Sindhi", "English"],
+        answer: "Urdu",
+    },
+    {
+        id: 2,
+        question: "Who was the founder of Pakistan?",
+        options: ["Fatima Jinnah", "Quaid-e-azam", "Red Cliffe", "Allama Iqbal"],
+        answer: "Quaid-e-azam",
+    },
+    {
+        id: 3,
+        question: "What is the capital of Pakistan?",
+        options: ["Multan", "Lahore", "Karachi", "Islamabad"],
+        answer: "Isalamabad"
+    },
+    {
+        id: 4,
+        question: "What is the name of the highest mountain peak of Pakistan?",
+        options: ["Tirch Mir", "Rakaposhi", "K2", "Nanga Parbat"],
+        answer: "K2"
+    },
+    {
+        id: 5,
+        question: "What is the national animal of Pakistan?",
+        options: ["Snow leopard", "Elephant", "Lion", "Markhor"],
+        answer: "Markhor"
+    },
+    {
+        id: 6,
+        question: "Which is the name of largest desert in Pakistan?",
+        options: ["Thar desert", "Thal desert", "Kharan Desert", "Cholistan desert"],
+        answer: "Thar desert"
+    },
+]
+ 
+export default qBank;
