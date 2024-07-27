d6478dbbcee421a2f02d5b4f61b12f25c2b9d7c6
hoorulain11 <hoorulainbarkatullahkhan218@gmail.com>
2024-07-27 14:58:32 +0500

    video part1 done :)

--- student-management-system/src/components/CreateStudent.jsx
+++ student-management-system/src/components/CreateStudent.jsx
@@ -6,19 +6,25 @@ import { db } from "../firebaseConfig";
 function CreateStudent() {
   const [name, setName] = useState('')
   const [age, setAge] = useState('')
+  const [isCreatingStudent, setIsCreatingStudent] = useState(false)
+  
 
      
    const handleSubmit = async(e)=> {
       e.preventDefault()
       try {
+        setIsCreatingStudent(true)
         await addDoc(collection(db,'students'),{
             name: name,
             age:Number(age)
         })
+
         setName('')
         setAge('')
+        setIsCreatingStudent(false)
       } catch (error) {
         console.log("error creating user:" , error)
+        setIsCreatingStudent(false)
       }
    }
 
@@ -29,7 +35,7 @@ function CreateStudent() {
 <form onSubmit={handleSubmit}>
     <input type="text" placeholder="enter student name" value={name} onChange={(e)=>setName(e.target.value)} required/>
     <input type="number" placeholder="enter student age" value={age} onChange={(e)=>setAge(e.target.value)} /> 
-    <button type="submit">create student</button>
+    <button type="submit">{isCreatingStudent ? 'creating...' : 'create student'}</button>
 </form>
     </>
   )
