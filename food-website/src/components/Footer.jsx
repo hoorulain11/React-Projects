3b7e687fc04042f45254521cd3f2dbc55bd13bee
hoorulain11 <hoorulainbarkatullahkhan218@gmail.com>
2024-08-03 21:35:05 +0500

    install react icons

--- food-website/src/components/Footer.jsx
+++ food-website/src/components/Footer.jsx
@@ -7,9 +7,9 @@ function Footer() {
     <>
      <div className='text-center font-bold text-black p-3'>
    <div className="icons flex justify-center gap-2 ">
-     <FaFacebook  size={"30px"}/>
-     <FaSquareInstagram size={"30px"}/>
-     <FaTwitter size={"30px"}/> 
+     <FaFacebook  size={"35px"}/>
+     <FaSquareInstagram size={"35px"}/>
+     <FaTwitter size={"35px"}/> 
    </div>
    <h2 className="font-semibold capitalize">All rights  Reserved Designed by <span className="text-pink-700 capitalize">hoor ul ain</span></h2>
 </div>
